247244f4d4bee77a0d0b5b4850f7467fcc4751f8
Maidul <u1703016@student.cuet.ac.bd>
2022-11-10 23:16:14 +0600

    [crt] Python_Basics/simple_calc.py: To demonstrate simple use of the unittest module (Contains add, subtract, multiply and division functions).
    [crt] Python_Basics/employee.py: To demostrate the use of setUp(), tearDown(), setUpClass(cls), tearDownClass(cls) of the unittest module (Collected from github repo of CoreySchafer and was slightly modified).

---
+++ Python_Basics/simple_calc.py
@@ -0,0 +1,22 @@
+# add x and y
+def add(x: int, y: int):
+# it is to say that we want values for x and y to be integer but even if it's float no problem, the computation will go through
+    return x + y
+
+# subtract y from x
+def subtract(x, y):
+    return x - y
+
+# multiply x and y
+def multiply(x, y):
+    return x * y
+
+# divide x by y
+def division(x, y):
+    if y == 0:
+        raise ZeroDivisionError("Can't divide by zero!")
+    return x / y
+
+
+if __name__ == "__main__":
+    print("3 divided by 2 is: \t", division(3, 2))
